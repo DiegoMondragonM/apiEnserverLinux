56a4cef8cfd952023cb8bcb94740a88e1b768d63
Diego Mondragon <momd0308@gmail.com>
2026-04-18 22:38:54 -0600

    Prueba Ahora ingresando un html

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/productos', require('./routes/productos.router.js'));
 
 
